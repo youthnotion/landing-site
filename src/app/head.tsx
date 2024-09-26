ad4c154390ce79c2b3165cc5a5966f6da65c1991
Abid Hasan <abidhasan.bh@outlook.com>
2024-09-26 16:11:15 +0600

    first commit

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Youth Notion</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="This SaaS Boilerplate and Starter Kit for Next.js is designed specifically for SaaS startups. It's a free resource complete with all the necessary integrations, pages, and components you require to build and launch a comprehensive SaaS website with robust features."
+      />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
